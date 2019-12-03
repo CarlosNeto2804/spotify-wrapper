0424fb373a0c119e5684a6e549bc921a3d335932
Carlos Neto <carloshtcursos@gmail.com>
2019-12-03 08:16:57 -0400

    Criando smoke tests

--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,5 +1,28 @@
-import { expect } from 'chai';
+import { expect } from "chai";
+import {
+  search,
+  searchAlbuns,
+  searchPlaylists,
+  searchTracks,
+  searchArtists
+} from "./../src/main";
 
-describe('Main', () => {
-
+describe("Spotify Wrapper", () => {
+  describe("smoke tests", () => {
+    it("shold exist the search method", () => {
+      expect(search).to.exist;
+    });
+    it("shold exist the searchAlbuns method", () => {
+      expect(searchAlbuns).to.exist;
+    });
+    it("shold exist the searchArtists method", () => {
+      expect(searchArtists).to.exist;
+    });
+    it("shold exist the searchTracks method", () => {
+      expect(searchTracks).to.exist;
+    });
+    it("shold exist the searchPlaylists method", () => {
+      expect(searchPlaylists).to.exist;
+    });
+  });
 });
